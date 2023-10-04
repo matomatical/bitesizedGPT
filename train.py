d99f96b3597222b1b55e431122dd30d9ae1cec30
Matthew Farrugia-Roberts <m@far.in.net>
2023-10-04 11:07:19 +0100

    add TPU support

--- train.py
+++ train.py
@@ -7,6 +7,16 @@ from model import complete, next_byte_cross_entropy_loss
 
 def train(device):
     print("chosen device:", device)
+    
+    XLA = (device == 'xla')
+    if XLA:
+        print("set environment variables expected by torch_xla...")
+        import os
+        os.environ['PJRT_DEVICE'] = "TPU"
+        print("importing torch_xla...")
+        import torch_xla.core.xla_model as xm
+        print("initialising default xla device...")
+        device = xm.xla_device()
 
     print("loading data...")
     data = ByteCorpus(
@@ -31,19 +41,24 @@ def train(device):
         model.parameters(),
         lr=0.001,
     )
+    
+    if XLA: xm.mark_step()
 
     print("training model...")
     for steps in tqdm.trange(num_training_steps):
+        if XLA: xm.mark_step()
         batch = data.get_training_batch(seq_length=128, batch_size=32)
         logits = model(batch)
         loss = next_byte_cross_entropy_loss(batch, logits)
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
+        if XLA: xm.mark_step()
 
         # evaluate periodically
         if steps % 100 == 0:
             tqdm.tqdm.write(f"eval at step {steps:>6d}")
+            if XLA: xm.mark_step()
             model.eval()
             # batch loss
             tqdm.tqdm.write(f"  training loss: {loss.item():>6.3f}")
@@ -58,12 +73,14 @@ def train(device):
                 ctn = complete(model, '"Elementary, my dear', max_bytes=32)
             tqdm.tqdm.write(f"  continuation:  {ctn!r}")
             model.train()
+            if XLA: xm.mark_step()
 
     print("done!")
     print("generating passage from model...")
     model.to('cpu')
     with torch.no_grad():
         ctn = complete(model, '"Elementary, my dear', max_bytes=256)
+    if XLA: xm.mark_step()
     for c in ctn:
         print(c, end="", flush=True)
         time.sleep(0.06125)
